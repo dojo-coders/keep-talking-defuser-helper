10ec53355ac31c8243b717b9f27a5d5daa87bed5
Gianpaulo M. Soares <jam_pow@hotmail.com>
2020-12-25 17:15:39 -0300

    adicionado modulo de fios simples com 5 fios

---
+++ src/241/modules/fios-simples/index.js
@@ -0,0 +1,77 @@
+
+const threeWires = (colors) => {
+    if (colors.indexOf('vermelho') === -1) {
+        return 2
+    }
+
+    if (colors[2] === 'branco') {
+        return 3
+    }
+
+    const blueWires = colors.filter(color => color === 'azul')
+
+    if (blueWires.length >= 2) {
+        return colors.lastIndexOf('azul') + 1
+    }
+
+    return 3
+}
+
+
+const fourWires = (serial, colors) => {
+
+    const redWires = colors.filter(color => color === 'vermelho')
+    const blueWires = colors.filter(color => color === 'azul')
+    const yellowWires = colors.filter(color => color === 'amarelo')
+
+    if (redWires.length >= 2 && serialIsOdd(serial)) {
+        return colors.lastIndexOf('vermelho') + 1
+    }
+    if (colors[3] === 'amarelo' && redWires.length === 0){
+        return 1
+    }
+    if (blueWires.length === 1){
+        return 1
+    }
+    if (yellowWires.length > 1){
+        return 4
+    }
+    return 2
+}
+
+const fiveWires = (serial, colors) => {
+    if (serialIsOdd(serial) && colors[4] === 'preto') {
+        return 4
+    }
+
+    const yellowWires = colors.filter(color => color === 'amarelo')
+    const redWires = colors.filter(color => color === 'vermelho')
+    
+    if (redWires.length === 1 && yellowWires.length > 1) {
+        return 1
+    }
+
+    if (colors.indexOf('preto') === -1) {
+        return 2
+    }
+
+    return 1
+}
+
+const serialIsOdd = (serial) => {
+    return parseInt(serial.substr(-1), 10) % 2 !== 0    
+}
+
+module.exports = (serial, ...colors) => {
+    switch (colors.length) {
+    case 3:
+        return threeWires(colors)
+    case 4:
+        return fourWires(serial, colors)
+    case 5:
+        return fiveWires(serial, colors)
+    default:
+        return 0
+    }
+
+}
---
+++ src/241/modules/fios-simples/index.test.js
@@ -0,0 +1,85 @@
+
+
+const fiosSimples = require('./index')
+
+const serialEven = 'assa52'
+const serialOdd = 'erere3'
+const serialUnd = undefined
+
+describe('fios-simples', () => {
+    describe('3 fios', () => {
+        it('Deve cortar o segundo fio se não houver vermelho', () => {
+            const resp = fiosSimples(serialEven, 'preto', 'amarelo', 'preto')
+            expect(resp).toBe(2)
+        })
+
+        it('Deve cortar o último fio se o último for branco', () => {
+            const resp = fiosSimples(serialEven, 'preto', 'vermelho', 'branco')
+            expect(resp).toBe(3)
+        })
+
+        it('Deve cortar o último fio azul se houver mais de um azul', () => {
+            const resp = fiosSimples(serialEven, 'azul', 'azul', 'vermelho')
+            expect(resp).toBe(2)
+        })
+
+        it('Deve cortar o último fio azul se houver mais de um azul (2)', () => {
+            const resp = fiosSimples(serialEven, 'azul', 'vermelho', 'azul')
+            expect(resp).toBe(3)
+        })
+
+        it('Deve cortar o último fio quando nenhuma das opções acima exitirem', () => {
+            const resp = fiosSimples(serialEven, 'vermelho', 'branco', 'azul')
+            expect(resp).toBe(3)
+        })
+    })
+
+    describe('4 fios', () => {
+        it('Deve cortar ultimo vermelho se mais de um fio vermelho e ultimo digito do serial for impar', () => {
+            const resp = fiosSimples(serialOdd, 'vermelho', 'branco', 'vermelho', 'preto')
+            expect(resp).toBe(3)
+        })
+
+        it('Deve cortar o primeiro fio se o ultimo for amarelo e nao houver vermelhos', () => {
+            const resp = fiosSimples(serialEven, 'preto', 'branco', 'preto', 'amarelo')
+            expect(resp).toBe(1)
+        })
+
+        it('Deve cortar o primeiro fio se houver exatamente um azul', () => {
+            const resp = fiosSimples(serialEven, 'azul', 'branco', 'preto', 'vermelho')
+            expect(resp).toBe(1)
+        })
+
+        it('Deve cortar o último fio se houver mais de um fio amarelo', () => {
+            const resp = fiosSimples(serialEven, 'amarelo', 'amarelo', 'vermelho', 'preto')
+            expect(resp).toBe(4)
+        })
+
+        it('Deve cortar o segundo fio quando nenhuma das opções acima exitirem', () => {
+            const resp = fiosSimples(serialEven, 'branco', 'branco', 'branco', 'branco')
+            expect(resp).toBe(2)
+        })
+    })
+
+    describe('5 fios', () => {
+      it('Deve cortar o quarto fio se o último fio for preto e o último dígito do número de série for ímpar', () => {
+        const resp = fiosSimples(serialOdd, 'branco', 'azul', 'amarelo', 'vermelho', 'preto')
+        expect(resp).toBe(4)
+      })
+
+      it('Deve cortar o primeiro fio se houver apenas um fio vermelho e mais de um amarelo', () => {
+        const resp = fiosSimples(serialOdd, 'branco', 'azul', 'amarelo', 'vermelho', 'amarelo')
+        expect(resp).toBe(1)
+      })
+
+      it('Deve cortar o segundo fio se não houver fios pretos', () => {
+        const resp = fiosSimples(serialOdd, 'branco', 'branco', 'branco', 'branco', 'branco')
+        expect(resp).toBe(2)
+      })
+
+      it('Deve cortar o primeiro fio se nenhuma das regras anteriores for atendida', () => {
+        const resp = fiosSimples(serialOdd, 'branco', 'branco', 'preto', 'branco', 'branco')
+        expect(resp).toBe(1)
+      })
+    })
+})
