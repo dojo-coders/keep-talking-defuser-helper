efc053d6ebc43e8296d02dcefb66952a9679a4f4
Gianpaulo M. Soares <jam_pow@hotmail.com>
2020-12-29 20:47:42 -0300

    finish simple wires module

--- src/622/modules/fios-simples/index.js
+++ src/622/modules/fios-simples/index.js
@@ -38,6 +38,44 @@ const fourWires = (serial, wires) => {
     return 2
 }
 
+const fiveWires = (serial, wires) => {
+    if (serialIsOdd(serial) && wires[4] === 'preto') {
+        return 4
+    }
+
+    const yellowWires = wires.filter(wire => wire === 'amarelo')
+    const redWires = wires.filter(wire => wire === 'vermelho')
+
+    if (redWires.length === 1 && yellowWires.length > 1) {
+        return 1
+    }
+
+    if (wires.indexOf('preto') === -1) {
+        return 2
+    }
+
+    return 1
+}
+
+const sixWires = (serial, wires) => {
+    if (serialIsOdd(serial) && wires.indexOf('amarelo') === -1) {
+        return 3
+    }
+
+    const yellowWires = wires.filter(wire => wire === 'amarelo')
+    const whiteWires = wires.filter(wire => wire === 'branco')
+
+    if (yellowWires.length === 1 && whiteWires.length > 1) {
+        return 4
+    }
+
+    if (wires.indexOf('vermelho') === -1) {
+        return 6
+    }
+
+    return 4
+}
+
 const serialIsOdd = (serial) => {
     return parseInt(serial.substr(-1), 10) % 2 !== 0    
 }
@@ -53,14 +91,16 @@ const serialIsOdd = (serial) => {
  * @param {string} [fio_6] - Cor do sexto fio
  * @returns {number} O número do fio a ser cortado
  */
-const simpleWires = (serial, ...colors) => {
-    switch (colors.length) {
+const simpleWires = (serial, ...wires) => {
+    switch (wires.length) {
     case 3:
         return threeWires(wires)
     case 4:
         return fourWires(serial, wires)
-    default:
-        return 0
+    case 5:
+        return fiveWires(serial, wires)
+    case 6:
+        return sixWires(serial, wires)
     }
 
 }
