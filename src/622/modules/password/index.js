3a54dfa5e92bcf7c2593e9c11138a96ae8929e38
Lucas Volcov <lucas.volcov@hotmail.com>
2020-12-30 02:15:53 +0000

    feat: password accepting different columns

--- src/622/modules/password/index.js
+++ src/622/modules/password/index.js
@@ -1,14 +1,10 @@
 const words = ['acesa', 'ajuda', 'amigo', 'antes', 'arcos', 'baile', 'balas', 'bispo', 'chefe','cheio', 'cinto', 'cravo', 'etapa', 'etnia', 'flora', 'lazer', 'legal', 'lugar', 'parte', 'parto', 'perto', 'porta', 'regra', 'resto', 'salve', 'sente', 'setor', 'sexta', 'tecla', 'tinta', 'torta', 'touro', 'trato', 'valer', 'veado']
 
 const password = (pass) => {
-    const rule = pass.map((col) => {
-        return '[' + col.join('') + ']'
-    }).join('')
+    const rule = pass.map((col) => col.length === 0 ? '.' : '[' + col.join('') + ']').join('')
     const regex = new RegExp('^' + rule)
-    const answer = words.filter((word) => {
-        return regex.test(word)
-    })
-  return answer
+    const answer = words.filter((word) => regex.test(word))
+    return answer
 }
-
+ 
 module.exports = password
