c89b3ce8046845280a2d52320106afb32c832d8e
gledsoncr <65854209+gledsoncr@users.noreply.github.com>
2021-01-05 09:10:17 -0300

    fix: removing the password module from this branch

--- src/622/modules/password/index.test.js
+++
@@ -1,7 +0,0 @@
-const password = require('./index')
-
-describe('password', () => {
-    it('Deve retornar a palavra ETAPA', () => {
-        expect(password([['m','e','p','v','x','l'],['y','d','n','b','t','z'],['a','s','l','k','z','b']])).toStrictEqual(['etapa'])
-    })
-})
