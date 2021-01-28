213de311c807a7b3597e4f7f73af486da2c753f1
Gianpaulo M. Soares <jam_pow@hotmail.com>
2021-01-28 20:55:24 -0300

    fix: export functions

--- src/622/modules/index.js
+++ src/622/modules/index.js
@@ -1,5 +1,17 @@
+const button = require('./button')
+const display = require('./display')
+const genius = require('./genius')
+const maze = require('./maze')
+const morse = require('./morse')
+const seqWires = require('./fios-sequenciados')
 const simpleWires = require('./fios-simples')
 
 module.exports = {
+  button,
+  display,
+  genius,
+  maze,
+  morse,
+  seqWires,
   simpleWires,
 }
