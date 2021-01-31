3236f22585096c181aca40fcd93c271f4d7cad42
Lucas Volcov <lucas.volcov@hotmail.com>
2021-01-31 11:33:11 +0000

    docs: added module documentation

--- src/622/modules/password/index.js
+++ src/622/modules/password/index.js
@@ -1,3 +1,9 @@
+/**
+ * Função que resolve o módulo password
+ * @param {...string} pass - Lista das letras apresentadas no módulo
+ * @returns {string[]} Palavra que é a resposta do módulo 
+ */
+
 const words = ['acesa', 'ajuda', 'amigo', 'antes', 'arcos', 'baile', 'balas', 'bispo', 'chefe','cheio', 'cinto', 'cravo', 'etapa', 'etnia', 'flora', 'lazer', 'legal', 'lugar', 'parte', 'parto', 'perto', 'porta', 'regra', 'resto', 'salve', 'sente', 'setor', 'sexta', 'tecla', 'tinta', 'torta', 'touro', 'trato', 'valer', 'veado']
 
 const password = (pass) => {
